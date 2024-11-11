2b708a3e23a4ed4a12183c7db703f804e8f284de
Dohyun Jung <serithemage@gmail.com>
2024-11-11 09:40:48 +0900

    Disable ESLint during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 빌드 시 ESLint 검사 비활성화
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
